49297e641e801fe2c4473a71deb197c02dd9a559
Victor Trujillo <vtrujillo2007@gmail.com>
2025-09-07 00:16:00 -0500

    first commit

---
+++ parcial1.js
@@ -0,0 +1,5 @@
+  var myCarousel = document.querySelector('#carouselExampleSlidesOnly');
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 1000, // 1 segundo
+    ride: 'carousel'
+  });
