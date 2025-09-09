e931bf4f1b38bed9139cbfc8777fabb3268cf1ec
unknown <u20241220242@usco.edu.co>
2025-09-09 08:14:02 -0500

    finish exam

--- parcial1.js
+++ parcial1.js
@@ -38,26 +38,62 @@
 
 
   //---Admin---
-    const form = document.getElementById("loginForm");
+    form.addEventListener("submit", function(event) {
+    event.preventDefault(); // evita recargar la página
 
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // evita que se recargue la página
-
-    // obtener valores de los inputs
+    // obtener valores
     const usuario = document.getElementById("usuario").value;
     const clave = document.getElementById("clave").value;
 
     // mostrar en consola
     console.log("Usuario ingresado:", usuario);
     console.log("Clave ingresada:", clave);
+
   });
 
-   // Botón Sign Up → redirigir a signup.html
-    document.getElementById("btnSignup").addEventListener("click", () => {
-      window.location.href = "SignUp.html";
-    });
 
-    // Botón Cancelar → redirigir a noticias.html
-    document.getElementById("btnCancel").addEventListener("click", () => {
-      window.location.href = "Noticias.html";
-    });
+  //---SignUp---
+  const signupForm = document.getElementById("signupForm");
+if (signupForm) {
+  signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Regex
+    const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phoneRegex = /^[0-9]{7,10}$/; // entre 7 y 10 dígitos
+    const estratoRegex = /^[1-6]$/; // solo 1 a 6
+
+    document.getElementById("signupForm").addEventListener("submit", function(e) {
+      e.preventDefault(); // Evita que recargue la página
+
+      const nombres = document.getElementById("nombres").value;
+      const apellidos = document.getElementById("apellidos").value;
+      const email = document.getElementById("email").value;
+      const telefono = document.getElementById("telefono").value;
+      const estrato = document.getElementById("estrato").value;
+      const fecha = document.getElementById("fecha").value;
+      const sangre = document.getElementById("sangre").value;
+      const genero = document.querySelector('input[name="genero"]:checked').value;
+
+      // Actividades seleccionadas
+      const actividades = Array.from(document.querySelectorAll('input[name="actividades"]:checked'))
+                               .map(cb => cb.value)
+                               .join(", ");
+
+      // Mostrar en alert
+      alert(
+        "Datos Registrados:\n" +
+        "Nombres: " + nombres + "\n" +
+        "Apellidos: " + apellidos + "\n" +
+        "Email: " + email + "\n" +
+        "Teléfono: " + telefono + "\n" +
+        "Estrato: " + estrato + "\n" +
+        "Fecha Nacimiento: " + fecha + "\n" +
+        "Grupo Sanguíneo: " + sangre + "\n" +
+        "Género: " + genero + "\n" +
+        "Actividades: " + actividades
+      );
+    }); 
+  });
+} 
