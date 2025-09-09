5a67d39c067a9e01b6a7cf27c764548f92e8ab08
unknown <u20241220242@usco.edu.co>
2025-09-09 06:48:42 -0500

    menu noticias

--- parcial1.js
+++ parcial1.js
@@ -1,5 +1,36 @@
+  
+  
+  
+  console.log("Hola, este es mi primer parcial de Frontend");
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   var myCarousel = document.querySelector('#carouselExampleSlidesOnly');
   var carousel = new bootstrap.Carousel(myCarousel, {
     interval: 1000, // 1 segundo
-    ride: 'carousel'
+  });
+
+
+  //---Admin---
+    const form = document.getElementById("loginForm");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // evita que se recargue la página
+
+    // obtener valores de los inputs
+    const usuario = document.getElementById("usuario").value;
+    const clave = document.getElementById("clave").value;
+
+    // mostrar en consola
+    console.log("Usuario ingresado:", usuario);
+    console.log("Clave ingresada:", clave);
   });
